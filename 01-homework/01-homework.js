cb590f33d35c1cce9a5bcf857c60de77adb7f2f3
Mary <prettmary19@gmail.com>
2021-02-20 18:40:50 +0200

    homework added

---
+++ 01-homework/01-homework.js
@@ -0,0 +1,40 @@
+const priceApple = 15.678;
+const priceOrange = 123.965;
+const pricePeach = 90.2345;
+console.log(`Ціна яблука ${priceApple}`);
+console.log(`Ціна апельсина ${priceOrange}`);
+console.log(`Ціна персика ${pricePeach}`);
+console.log(
+  `Максимальна ціна ${Math.max(priceApple, priceOrange, pricePeach)}`
+);
+console.log(`Мінімальна ціна ${Math.min(priceApple, priceOrange, pricePeach)}`);
+const totalAmount = priceApple + priceOrange + pricePeach;
+console.log(`Сума всіх товарів ${totalAmount}`);
+const priceAppleInteger = Math.trunc(priceApple);
+const priceOrangeInteger = Math.trunc(priceOrange);
+const pricePeachInteger = Math.trunc(pricePeach);
+const totalAmountInteger =
+  priceAppleInteger + priceOrangeInteger + pricePeachInteger;
+console.log(
+  `Сума товарів округлена до сотень ${
+    Math.round(totalAmountInteger / 100) * 100
+  }`
+);
+if (totalAmountInteger % 2 === 0) {
+  console.log(`Сума є парним числом? ${true}`);
+} else {
+  console.log(`Сума є непарним числом? ${false}`);
+}
+const clientCash = 500;
+console.log(`Решта від 500 ${clientCash - totalAmount}`);
+console.log(
+  `Середнє значення цін ${((priceApple + priceOrange + pricePeach) / 3).toFixed(
+    2
+  )}`
+);
+const randomDiscount = Math.floor(Math.random() * 101);
+const discount = (totalAmount / 100) * randomDiscount;
+const discountedAmount = (totalAmount - discount).toFixed(2);
+console.log(`Сума до оплати зі знижкою ${discountedAmount}`);
+const costPrice = totalAmount / 2;
+console.log(`Чистий прибуток ${costPrice - discount}`);
