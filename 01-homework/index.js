3fe0e3862f721281f0203442fac37881ac3e59c9
Mary <prettmary19@gmail.com>
2021-02-23 08:39:40 +0200

    adding changes

--- 01-homework/index.js
+++ 01-homework/index.js
@@ -4,10 +4,12 @@ const pricePeach = 90.2345;
 console.log(`Ціна яблука ${priceApple}`);
 console.log(`Ціна апельсина ${priceOrange}`);
 console.log(`Ціна персика ${pricePeach}`);
+const maxPrice = Math.max(priceApple, priceOrange, pricePeach);
 console.log(
-  `Максимальна ціна ${Math.max(priceApple, priceOrange, pricePeach)}`
+  `Максимальна ціна ${maxPrice}`
 );
-console.log(`Мінімальна ціна ${Math.min(priceApple, priceOrange, pricePeach)}`);
+const minPrice = Math.min(priceApple, priceOrange, pricePeach);
+console.log(`Мінімальна ціна ${minPrice}`);
 const totalAmount = priceApple + priceOrange + pricePeach;
 console.log(`Сума всіх товарів ${totalAmount}`);
 const priceAppleInteger = Math.trunc(priceApple);
@@ -16,10 +18,9 @@ const pricePeachInteger = Math.trunc(pricePeach);
 const totalAmountInteger =
   priceAppleInteger + priceOrangeInteger + pricePeachInteger;
 console.log(`Сума товарів безкопійок ${totalAmountInteger}`);
+const amountRoundedToHundreds = Math.round(totalAmountInteger / 100) * 100;
 console.log(
-  `Сума товарів округлена до сотень ${
-    Math.round(totalAmountInteger / 100) * 100
-  }`
+  `Сума товарів округлена до сотень ${amountRoundedToHundreds}`
 );
 if (totalAmountInteger % 2 === 0) {
   console.log(`Сума є парним числом? ${true}`);
@@ -27,15 +28,16 @@ if (totalAmountInteger % 2 === 0) {
   console.log(`Сума є непарним числом? ${false}`);
 }
 const clientCash = 500;
-console.log(`Решта від 500 ${clientCash - totalAmount}`);
+const restPayment = clientCash - totalAmount;
+console.log(`Решта від 500 ${restPayment}`);
+const averageValueOfPrices = ((priceApple + priceOrange + pricePeach) / 3).toFixed(2);
 console.log(
-  `Середнє значення цін ${((priceApple + priceOrange + pricePeach) / 3).toFixed(
-    2
-  )}`
+  `Середнє значення цін ${averageValueOfPrices}`
 );
 const randomDiscount = Math.floor(Math.random() * 101);
 const discount = (totalAmount / 100) * randomDiscount;
 const discountedAmount = (totalAmount - discount).toFixed(2);
 console.log(`Сума до оплати зі знижкою ${discountedAmount}`);
 const costPrice = totalAmount / 2;
-console.log(`Чистий прибуток ${costPrice - discount}`);
+const profit = costPrice - discount;
+console.log(`Чистий прибуток ${profit}`);
