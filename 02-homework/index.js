94e43c9f2430b91ec49b7781d16e1adf99d4d9c2
Mary <prettmary19@gmail.com>
2021-02-24 09:34:48 +0200

    adding changes

--- 02-homework/index.js
+++ 02-homework/index.js
@@ -2,17 +2,19 @@ let numberN;
 let numberM;
 do {
     getNumberN = +prompt('Введіть перше число N. Якщо введете НЕ число, доведеться вводити повторно.');
-} while (Number.isNaN(getNumberN));
+    numberN = parseInt(getNumberN, 10);
+} while (Number.isNaN(numberN));
 do {
     getNumberM = +prompt('Введіть друге число M. Якщо введете НЕ число, доведеться вводити повторно.');
-} while (Number.isNaN(getNumberM));
+    numberM = parseInt(getNumberM, 10);
+} while (Number.isNaN(numberM));
 let sumOfNumbers = 0;
 const skipEvenNumbers = confirm('Чи потрібно пропустити парні числа?');
 console.log(skipEvenNumbers);
-for (let i = getNumberN; i <= getNumberM; i++){
+for (let i = numberN; i <= numberM; i++){
     if (skipEvenNumbers && (i % 2 == 0)) {
         continue;
     }
     sumOfNumbers += i;
 }
-alert(`Сума чисел від ${getNumberN} до ${getNumberM} дорівнює ${sumOfNumbers}`)
+alert(`Сума чисел від ${numberN} до ${numberM} дорівнює ${sumOfNumbers}`)
