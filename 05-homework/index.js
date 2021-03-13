48cc266549a98e4bca5dd8f00e559b80d94710e1
Mary <56792345+marychain@users.noreply.github.com>
2021-03-13 16:27:48 +0200

    adding changes

--- 05-homework/index.js
+++ 05-homework/index.js
@@ -1,36 +1,24 @@
-function getRandomArray(length, min, max) {
-    const randomArray = [];
-    for (let index = 0; index < length; index++) {
-        randomArray[index] = [Math.floor(Math.random() * (max - min + 1)) + min];
-    }
-    return randomArray;
-}
-console.log("Масив випадкових чисел:", getRandomArray(15, 1, 100));
+const getRandomArray = (min, max, length) =>
+    Array(length).fill(0).map(item => Math.floor(Math.random() * (max - min + 1) + min))
+console.log("Масив випадкових чисел:", getRandomArray(1, 100, 15));
 function getModa(...numbers) {
     let moda=0;
+    let counter = 0;
+    let maxCounterValue = 0;
 	numbers.filter(number => Number.isInteger(number) == true).map((number) => {
-        let counter = 0;
-        let maxCounterValue = 0;
         for (let i = 0; i < numbers.length; i++) {
-            if (number == numbers[i]) 
+            if (number === numbers[i]) 
                 counter++;
-            if(counter > maxCounterValue){
-                maxCounterValue = counter;
-                moda = number;
-            }
-        }
-        counter = 0;
+                  maxCounterValue = counter;
+                  moda = numbers[i];
+               if (counter <= maxCounterValue)
+               break;
+        } 
     });
     return moda;
 }
-console.log("Мода масиву:", getModa(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-function getAverage(...numbers) {
-    numbers= numbers.filter(number => Number.isInteger(number) == true);
-    total = numbers.reduce((total, number) => {
-        return total + number;
-    }, 0);
-    return total / numbers.length;
-}
+console.log("Мода масиву:", getModa(6, 6, 6, 6, 6, 2, 55, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+const getAverage = (...numbers) => numbers.reduce((total, number) => total + number) / numbers.length;
 console.log("Середнє арифметичне переданих аргументів: ", getAverage(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 function getMedian(...numbers) {
 	numbers = numbers.filter(number => Number.isInteger(number) == true);
@@ -39,25 +27,17 @@ function getMedian(...numbers) {
     if (numbers.length % 2) {
         return numbers[halfArray];
     } else {
-        return (numbers[halfArray] + numbers[halfArray] + 1) / 2;
+        return (numbers[halfArray-1] + numbers[halfArray]) / 2;
   }
 }
-console.log("Медіана масиву:",getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
-function filterEvenNumbers(...numbers) {
-    const filterNumbers = numbers.filter(number => (number % 2) !== 0);
-    return filterNumbers;
-}
+console.log("Медіана масиву:", getMedian(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+console.log("Медіана масиву:",getMedian(1, 2, 3, 4));
+const filterEvenNumbers = (...numbers) => numbers.filter(number => number % 2 !== 0)
 console.log("Масив з відфільтрованими парними числами:",filterEvenNumbers(1, 2, 3, 4, 5, 6));
-function countPositiveNumbers(...numbers) {
-    const positiveNumbers = numbers.filter(number => number > 0);
-    return positiveNumbers.length;
-}
-console.log("Кількість чисел більших за 0:",countPositiveNumbers(1, -2, 3, -4, -5, 6));
-function getDividedByFive(...numbers) {
-    const dividedByFive = numbers.filter(number => (number % 5) === 0);
-    return dividedByFive;
-}
-console.log("Масив чисел, що діляться на 5:",getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
+const countPositiveNumbers = (...numbers) => numbers.filter(number => number > 0);
+console.log("Кількість чисел більших за 0:",countPositiveNumbers(1, -2, 3, -4, -5, 6).length);
+const getDividedByFive = (...numbers) => numbers.filter(number => number % 5 === 0);
+console.log("Масив чисел, що діляться на 5:", getDividedByFive(6, 2, 55, 11, 78, 2, 55, 77, 57, 87, 23, 2, 56, 3, 2));
 function replaceBadWords(string) {
     const wordsArray = string.split(' ');
     const badWords = ["shit", "fuck"];
